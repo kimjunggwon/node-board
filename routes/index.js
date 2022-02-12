f70e09f48a22ec83a12820c92d77505459de1f40
김중권 <gimjung-gwon@gimjung-gwon-ui-MacBookPro.local>
2022-02-13 00:47:39 +0900

    node-default-setting

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const controller = require('./controller/controller');
+
+const router = express.Router();
+
+router.get('/', controller.index);
+
+module.exports = router;
+
+  
