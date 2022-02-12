dc724fe2452b25d94968080972b376a395b2f8bf
김중권 <gimjung-gwon@gimjung-gwon-ui-MacBookPro.local>
2022-02-12 22:56:58 +0900

    model-setting1

---
+++ models/notice.js
@@ -0,0 +1,28 @@
+const Sequelize = require('sequelize');
+
+module.exports = class Notice extends Sequelize.Model {
+    static init(sequelize){
+        return super.init({
+            title: {
+                type: Sequelize.STRING(100),
+                allowNull: false,
+            },
+            content: {
+                type: Sequelize.TEXT,
+                allowNull: false,
+            }
+        }, {
+            sequelize,
+            timestamps: true,
+            paranoid: true,
+            modelName: 'Notice',
+            tableName: 'notices',
+            charset: 'utf8',
+            collate: 'utf8_genrenal_ci',
+        })
+    }
+
+    static associate(db){
+        
+    }
+}
